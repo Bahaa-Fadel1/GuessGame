99c102d2a904ae4dee65426488f409cd1a739e0e
Bahaa-Fadel1 <bahaazaki652@gmail.com>
2026-01-07 16:29:54 +0200

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,7 +1,7 @@
 <?php
 declare(strict_types=1);
 session_start();
-require_once DIR."/storage.php";
+require_once __DIR__."/storage.php";
 function h(string $s):string{return htmlspecialchars($s,ENT_QUOTES,'UTF-8');}
 
 $levels=[
